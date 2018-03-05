83febe5bfcd22ec64d5c4227d0c5e33f2cd717f8
Levi Sigworth <dev@xiah.io>
2018-03-05 16:26:03 -0700

    Initial slides for Enabling Content Editors with Paragraphs and Atomic Design.

--- js/reveal.js
+++ js/reveal.js
@@ -67,13 +67,13 @@
 			progress: true,
 
 			// Display the page number of the current slide
-			slideNumber: false,
+			slideNumber: true,
 
 			// Determine which displays to show the slide number on
 			showSlideNumber: 'all',
 
 			// Push each slide change to the browser history
-			history: false,
+			history: true,
 
 			// Enable keyboard shortcuts for navigation
 			keyboard: true,
@@ -159,7 +159,7 @@
 			focusBodyOnPageVisibilityChange: true,
 
 			// Transition style
-			transition: 'slide', // none/fade/slide/convex/concave/zoom
+			transition: 'convex', // none/fade/slide/convex/concave/zoom
 
 			// Transition speed
 			transitionSpeed: 'default', // default/fast/slow
